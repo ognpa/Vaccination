7b000cf2d05bd2d552d521ee92b676bd11b5a09c
ognpa <priya.ananthram@gmail.com>
2018-09-18 21:19:20 +1000

    first commit

---
+++ src/socialprivilege_vaccination.R
@@ -0,0 +1,28 @@
+setwd("/Users/priya/Downloads/Vaccination")
+df<-read.csv("./cleaned_data/immunization_with_everything.csv")
+head(df)
+#Remove NP 
+df=subset(df,df$pc_immun!='NP')
+df$pc_immun<-factor(df$pc_immun)
+
+plot(table(df$Rank.within.Australia,df$pc_immun))
+
+colnames(df)
+summary(df$Rank.within.Australia...Decile)
+summary(df$Score)
+
+d<-(prop.table(table(df$Rank.within.Australia...Decile,df$pc_immun),1)*100)
+library(ggplot2)
+head(d)
+
+spineplot(as.factor(df$Rank.within.Australia...Decile), as.factor(df$pc_immun))
+library(ggplot2)
+library(vcd)
+
+
+mosaicplot(d,las = 2,     dir = c("h", "v"),
+           color = "skyblue"
+           ,ylab="Rank within australia",
+           xlab="Percentage immunized",
+           off=30,main='Social privilege and vaccination',
+           border = "chocolate") 
